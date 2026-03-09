33e31ad5ab6516f1eec4dc0ece9c409dbbbff5d4
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-03-09 15:32:33 -0300

    nada

---
+++ 12.arrow-function.js
@@ -0,0 +1,13 @@
+// função.
+// arrow function
+
+const somar = (a, b) => {
+    return a + b
+}
+
+const substrair =(a, b) => a - b
+
+const soma = somar(2, 3)
+
+console.log("soma: ${soma}")
+console.log('subtração: ${subtrair}')
