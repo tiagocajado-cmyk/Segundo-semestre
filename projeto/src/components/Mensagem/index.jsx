47c35c02c510dff587a8117ded90a194236d8ed7
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-03-26 16:54:25 -0300

    ooooo

---
+++ projeto/src/components/Mensagem/index.jsx
@@ -0,0 +1,13 @@
+import './style.css'
+
+function Mensagem() {
+    return (
+        // Comandos HTML.
+        <div className='mensagem'>
+            <h1>Coponente</h1>
+            <p>Este componente é uma mensagem simples.</p>
+        </div>
+    )
+}
+
+export default Mensagem
